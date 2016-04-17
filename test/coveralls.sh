e1c2d94d7130461603e5d1382a12b8c21987f95a
Ruediger Gad <r.c.g@gmx.de>
2016-04-17 09:13:22 +0200

    Do not instrument bowerick.java-interfaces for cloverage as this seems to cause errors.

--- test/coveralls.sh
+++ test/coveralls.sh
@@ -1,4 +1,4 @@
 COVERALLS_URL='https://coveralls.io/api/v1/jobs'
-lein2 with-profile cloverage cloverage -e "bowerick.main" -o cov --coveralls
+lein2 with-profile cloverage cloverage -e "bowerick.main" -e "bowerick.java-interfaces" -o cov --coveralls
 curl -F 'json_file=@cov/coveralls.json' "$COVERALLS_URL"
 
