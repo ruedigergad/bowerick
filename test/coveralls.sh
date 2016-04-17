4952b79c2019c665bf24eb44245312b6c3e11fd3
Ruediger Gad <r.c.g@gmx.de>
2016-04-17 09:09:22 +0200

    Add cloverage specific profile.

--- test/coveralls.sh
+++ test/coveralls.sh
@@ -1,4 +1,4 @@
 COVERALLS_URL='https://coveralls.io/api/v1/jobs'
-lein2 cloverage -e "bowerick.main" -o cov --coveralls
+lein2 with-profile cloverage cloverage -e "bowerick.main" -o cov --coveralls
 curl -F 'json_file=@cov/coveralls.json' "$COVERALLS_URL"
 
