e9b7cd6eaf9fbeb02a31f70edfb2360e94ce263f
Ruediger Gad <r.c.g@gmx.de>
2016-04-19 19:55:37 +0200

    Add benchmarks for nippy with compression.

--- benchmark/bowerick/benchmarks.clj
+++ benchmark/bowerick/benchmarks.clj
@@ -94,6 +94,42 @@
       (close producer)
       (close consumer))))
 
+(deftest ^:benchmark pooled-nippy-lz4-string-transmission-benchmarks
+  (doseq [n [1 1 1 1 2 3 4 6 8 10 15 20 30 40 50 75 100 150 200 300 400 500 750 1000]]
+    (println (str "Running benchmark: pooled-nippy-lz4-string-transmission-benchmark-" n))
+    (let [producer (create-pooled-nippy-producer *local-jms-server* test-topic n {:compressor taoensso.nippy/lz4-compressor})
+          consume-fn (fn [_])
+          consumer (create-pooled-nippy-consumer *local-jms-server* test-topic consume-fn)]
+      (cc/with-progress-reporting
+        (cc/quick-bench
+          (producer "foo-string")))
+      (close producer)
+      (close consumer))))
+
+(deftest ^:benchmark pooled-nippy-lzma2-string-transmission-benchmarks
+  (doseq [n [1 1 1 1 2 3 4 6 8 10 15 20 30 40 50 75 100 150 200 300 400 500 750 1000]]
+    (println (str "Running benchmark: pooled-nippy-lzma2-string-transmission-benchmark-" n))
+    (let [producer (create-pooled-nippy-producer *local-jms-server* test-topic n {:compressor taoensso.nippy/lzma2-compressor})
+          consume-fn (fn [_])
+          consumer (create-pooled-nippy-consumer *local-jms-server* test-topic consume-fn)]
+      (cc/with-progress-reporting
+        (cc/quick-bench
+          (producer "foo-string")))
+      (close producer)
+      (close consumer))))
+
+(deftest ^:benchmark pooled-nippy-snappy-string-transmission-benchmarks
+  (doseq [n [1 1 1 1 2 3 4 6 8 10 15 20 30 40 50 75 100 150 200 300 400 500 750 1000]]
+    (println (str "Running benchmark: pooled-nippy-snappy-string-transmission-benchmark-" n))
+    (let [producer (create-pooled-nippy-producer *local-jms-server* test-topic n {:compressor taoensso.nippy/snappy-compressor})
+          consume-fn (fn [_])
+          consumer (create-pooled-nippy-consumer *local-jms-server* test-topic consume-fn)]
+      (cc/with-progress-reporting
+        (cc/quick-bench
+          (producer "foo-string")))
+      (close producer)
+      (close consumer))))
+
 (deftest ^:benchmark pooled-lzf-string-transmission-benchmarks
   (doseq [n [1 1 1 1 2 3 4 6 8 10 15 20 30 40 50 75 100 150 200 300 400 500 750 1000]]
     (println (str "Running benchmark: pooled-lzf-string-transmission-benchmark-" n))
