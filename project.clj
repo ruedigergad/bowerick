8a0f470a7ab67d18aecba7278ad14b0a3cb7d57f
Ruediger Gad <r.c.g@gmx.de>
2016-04-19 18:37:08 +0200

    Organize dependencies.

--- project.clj
+++ project.clj
@@ -1,18 +1,18 @@
 (defproject bowerick "1.99.3"
   :description "Toolkit for using JMS with Clojure."
-  :dependencies [[com.taoensso/nippy "2.11.1"]
-                 [org.clojure/clojure "1.8.0"]
-                 [org.clojure/tools.cli "0.2.4"]
+  :dependencies [[cheshire "5.5.0"]
+                 [clj-assorted-utils "1.12.0"]
                  [com.ning/compress-lzf "1.0.3"]
+                 [com.taoensso/nippy "2.11.1"]
                  [org.apache.activemq/activemq-broker "5.13.2"]
                  [org.apache.activemq/activemq-client "5.13.2"]
                  [org.apache.activemq/activemq-jaas "5.13.2"]
                  [org.apache.activemq/activemq-openwire-legacy "5.13.2"]
                  [org.apache.activemq/activemq-stomp "5.13.2"]
+                 [org.clojure/clojure "1.8.0"]
+                 [org.clojure/tools.cli "0.2.4"]
                  [org.fusesource.stompjms/stompjms-client "1.19"]
-                 [org.slf4j/slf4j-simple "1.7.10"]
-                 [clj-assorted-utils "1.12.0"]
-                 [cheshire "5.5.0"]]
+                 [org.slf4j/slf4j-simple "1.7.10"]]
   :license {:name "Eclipse Public License (EPL) - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"
             :distribution :repo
