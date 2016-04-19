4439b87fe5875c666ee2fb32a55222e5749d974f
Ruediger Gad <r.c.g@gmx.de>
2016-04-19 22:23:37 +0200

    Add benchmarks for carbonite and carbonite-lzf.

--- benchmark/bowerick/benchmarks.clj
+++ benchmark/bowerick/benchmarks.clj
@@ -142,3 +142,27 @@
       (close producer)
       (close consumer))))
 
+(deftest ^:benchmark pooled-carbonite-string-transmission-benchmarks
+  (doseq [n [1 1 1 1 2 3 4 6 8 10 15 20 30 40 50 75 100 150 200 300 400 500 750 1000]]
+    (println (str "Running benchmark: pooled-carbonite-string-transmission-benchmark-" n))
+    (let [producer (create-pooled-carbonite-producer *local-jms-server* test-topic n)
+          consume-fn (fn [_])
+          consumer (create-pooled-carbonite-consumer *local-jms-server* test-topic consume-fn)]
+      (cc/with-progress-reporting
+        (cc/quick-bench
+          (producer "foo-string")))
+      (close producer)
+      (close consumer))))
+
+(deftest ^:benchmark pooled-carbonite-lzf-string-transmission-benchmarks
+  (doseq [n [1 1 1 1 2 3 4 6 8 10 15 20 30 40 50 75 100 150 200 300 400 500 750 1000]]
+    (println (str "Running benchmark: pooled-carbonite-lzf-string-transmission-benchmark-" n))
+    (let [producer (create-pooled-carbonite-lzf-producer *local-jms-server* test-topic n)
+          consume-fn (fn [_])
+          consumer (create-pooled-carbonite-lzf-consumer *local-jms-server* test-topic consume-fn)]
+      (cc/with-progress-reporting
+        (cc/quick-bench
+          (producer "foo-string")))
+      (close producer)
+      (close consumer))))
+
