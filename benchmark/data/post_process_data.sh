d8dc33c1879ab439b8b2a93830f113c605b3c410
Ruediger Gad <r.c.g@gmx.de>
2016-04-20 20:25:56 +0200

    Add test benchmark data.

---
+++ benchmark/data/post_process_data.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+DATA_SET_NAME=$1
+
+grep -e "Running benchmark" -e "Execution time mean" ${DATA_SET_NAME}.raw | sed '$!N;s/\n/ /' | awk '{print $9,($5 * ($6=="ns" ? 1 : ($6=="µs" ? 1000 : 1000000)))}' | grep -v -e single -e simple -e "^ " -e "^ms" -e "^µs" | sed 's/-\([0-9]\)/ \1/g' | awk '{print $2,$3 > ($1 ".out_tmp")}'
+
+mkdir $DATA_SET_NAME
+
+for i in *.out_tmp
+do
+    tail -n +4 $i > ${DATA_SET_NAME}/$(basename -s .out_tmp $i).data
+done
+
+rm -f *.out_tmp
+
