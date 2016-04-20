27492020c92f9839a4428958336261e6efc4b9e9
Ruediger Gad <r.c.g@gmx.de>
2016-04-20 21:30:27 +0200

    Rework plots.

--- benchmark/results/create_plot.sh
+++ benchmark/results/create_plot.sh
@@ -9,12 +9,13 @@ cd ${DATA_SET_NAME}
 
 for i in *${BENCHMARK_SET}*.data
 do
-    echo '    "'$(basename $i)'" using 1:2 with linespoints lw 2.5 pt 1 ps 1.5,\' >> ${BENCHMARK_SET}.gpl
+    echo '    "'$(basename $i)'" using 1:($2/1000) with linespoints lw 2.5 ps 1 title "'$(basename -s .data $i)'",\' >> ${BENCHMARK_SET}.gpl
 done
 
 echo "" >> ${BENCHMARK_SET}.gpl
 
 gnuplot -e 'set output "'${BENCHMARK_SET}'.pdf"' ${BENCHMARK_SET}.gpl
+convert -flatten -density 150 -quality 100 -sharpen 0x1.0 ${BENCHMARK_SET}.pdf ${BENCHMARK_SET}.png
 
 cd ..
 
