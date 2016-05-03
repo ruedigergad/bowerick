54cd80a55a955d4945f350275ce35822d32e6c54
Ruediger Gad <r.c.g@gmx.de>
2016-05-03 07:00:37 +0200

    Add results.

--- benchmark/bowerick/benchmarks.clj
+++ benchmark/bowerick/benchmarks.clj
@@ -77,7 +77,7 @@
     (let [consumer (consumer-factory-fn *local-jms-server* test-topic identity)
           producer (producer-factory-fn *local-jms-server* test-topic n)]
       (cc/with-progress-reporting
-        (cc/quick-bench
+        (cc/bench
           (producer data)))
       (close producer)
       (close consumer))))
