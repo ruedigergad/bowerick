c999056c15501943addf819fa9c13e68f8d93e0a
Ruediger Gad <r.c.g@gmx.de>
2016-05-07 12:06:56 +0200

    Clean up benchmark tests.

--- benchmark/bowerick/benchmarks.clj
+++ benchmark/bowerick/benchmarks.clj
@@ -26,50 +26,6 @@
 
 (use-fixtures :each benchmark-fixture)
 
-;(deftest ^:benchmark simple-string-transmission-benchmark
-;  (println "Running benchmark: simple-string-transmission-benchmark")
-;  (let [producer (create-producer *local-jms-server* test-topic)
-;        consume-fn (fn [_])
-;        consumer (create-consumer *local-jms-server* test-topic consume-fn)]
-;    (cc/with-progress-reporting
-;      (cc/quick-bench
-;        (producer "foo-string")))
-;    (close producer)
-;    (close consumer)))
-;
-;(deftest ^:benchmark pooled-string-transmission-benchmark-10-single
-;  (println "Running benchmark: pooled-string-transmission-benchmark-10-single")
-;  (let [producer (create-pooled-producer *local-jms-server* test-topic 10)
-;        consume-fn (fn [_])
-;        consumer (create-pooled-consumer *local-jms-server* test-topic consume-fn)]
-;    (cc/with-progress-reporting
-;      (cc/quick-bench
-;        (producer "foo-string")))
-;    (close producer)
-;    (close consumer)))
-;
-;(deftest ^:benchmark pooled-nippy-string-transmission-benchmark-10-single
-;  (println "Running benchmark: pooled-nippy-string-transmission-benchmark-10-single")
-;  (let [producer (create-pooled-nippy-producer *local-jms-server* test-topic 10)
-;        consume-fn (fn [_])
-;        consumer (create-pooled-nippy-consumer *local-jms-server* test-topic consume-fn)]
-;    (cc/with-progress-reporting
-;      (cc/quick-bench
-;        (producer "foo-string")))
-;    (close producer)
-;    (close consumer)))
-;
-;(deftest ^:benchmark pooled-nippy-lzf-string-transmission-benchmark-10-single
-;  (println "Running benchmark: pooled-nippy-lzf-string-transmission-benchmark-10-single")
-;  (let [producer (create-pooled-nippy-lzf-producer *local-jms-server* test-topic 10)
-;        consume-fn (fn [_])
-;        consumer (create-pooled-nippy-lzf-consumer *local-jms-server* test-topic consume-fn)]
-;    (cc/with-progress-reporting
-;      (cc/quick-bench
-;        (producer "foo-string")))
-;    (close producer)
-;    (close consumer)))
-
 (defn run-benchmarks
   [description producer-factory-fn consumer-factory-fn data]
   (doseq [n [1 1 2 4 10 20 50 100 200 400 750 1000]]
@@ -82,64 +38,64 @@
       (close producer)
       (close consumer))))
 
-(deftest ^:benchmark pooled-string-transmission-benchmarks
-  (run-benchmarks
-    "pooled-string-transmission"
-    create-pooled-producer
-    create-pooled-consumer
-    "foo-string"))
-
-(deftest ^:benchmark pooled-nippy-string-transmission-benchmarks
-  (run-benchmarks
-    "pooled-nippy-string-transmission"
-    create-pooled-nippy-producer
-    create-pooled-nippy-consumer
-    "foo-string"))
-
-(deftest ^:benchmark pooled-nippy-lz4-string-transmission-benchmarks
-  (run-benchmarks
-    "pooled-nippy-lz4-string-transmission"
-    (fn [url ep n]
-      (create-pooled-nippy-producer url ep n {:compressor taoensso.nippy/lz4-compressor}))
-    create-pooled-nippy-consumer
-    "foo-string"))
-
-(deftest ^:benchmark pooled-nippy-lzma2-string-transmission-benchmarks
-  (run-benchmarks
-    "pooled-nippy-lzma2-string-transmission"
-    (fn [url ep n]
-      (create-pooled-nippy-producer url ep n {:compressor taoensso.nippy/lzma2-compressor}))
-    create-pooled-nippy-consumer
-    "foo-string"))
-
-(deftest ^:benchmark pooled-nippy-snappy-string-transmission-benchmarks
-  (run-benchmarks
-    "pooled-nippy-snappy-string-transmission"
-    (fn [url ep n]
-      (create-pooled-nippy-producer url ep n {:compressor taoensso.nippy/snappy-compressor}))
-    create-pooled-nippy-consumer
-    "foo-string"))
-
-(deftest ^:benchmark pooled-nippy-lzf-string-transmission-benchmarks
-  (run-benchmarks
-    "pooled-nippy-lzf-string-transmission"
-    create-pooled-nippy-lzf-producer
-    create-pooled-nippy-lzf-consumer
-    "foo-string"))
-
-(deftest ^:benchmark pooled-carbonite-string-transmission-benchmarks
-  (run-benchmarks
-    "pooled-carbonite-string-transmission"
-    create-pooled-carbonite-producer
-    create-pooled-carbonite-consumer
-    "foo-string"))
-
-(deftest ^:benchmark pooled-carbonite-lzf-string-transmission-benchmarks
-  (run-benchmarks
-    "pooled-carbonite-lzf-string-transmission"
-    create-pooled-carbonite-lzf-producer
-    create-pooled-carbonite-lzf-consumer
-    "foo-string"))
+;(deftest ^:benchmark pooled-string-transmission-benchmarks
+;  (run-benchmarks
+;    "pooled-string-transmission"
+;    create-pooled-producer
+;    create-pooled-consumer
+;    "foo-string"))
+;
+;(deftest ^:benchmark pooled-nippy-string-transmission-benchmarks
+;  (run-benchmarks
+;    "pooled-nippy-string-transmission"
+;    create-pooled-nippy-producer
+;    create-pooled-nippy-consumer
+;    "foo-string"))
+;
+;(deftest ^:benchmark pooled-nippy-lz4-string-transmission-benchmarks
+;  (run-benchmarks
+;    "pooled-nippy-lz4-string-transmission"
+;    (fn [url ep n]
+;      (create-pooled-nippy-producer url ep n {:compressor taoensso.nippy/lz4-compressor}))
+;    create-pooled-nippy-consumer
+;    "foo-string"))
+;
+;(deftest ^:benchmark pooled-nippy-lzma2-string-transmission-benchmarks
+;  (run-benchmarks
+;    "pooled-nippy-lzma2-string-transmission"
+;    (fn [url ep n]
+;      (create-pooled-nippy-producer url ep n {:compressor taoensso.nippy/lzma2-compressor}))
+;    create-pooled-nippy-consumer
+;    "foo-string"))
+;
+;(deftest ^:benchmark pooled-nippy-snappy-string-transmission-benchmarks
+;  (run-benchmarks
+;    "pooled-nippy-snappy-string-transmission"
+;    (fn [url ep n]
+;      (create-pooled-nippy-producer url ep n {:compressor taoensso.nippy/snappy-compressor}))
+;    create-pooled-nippy-consumer
+;    "foo-string"))
+;
+;(deftest ^:benchmark pooled-nippy-lzf-string-transmission-benchmarks
+;  (run-benchmarks
+;    "pooled-nippy-lzf-string-transmission"
+;    create-pooled-nippy-lzf-producer
+;    create-pooled-nippy-lzf-consumer
+;    "foo-string"))
+;
+;(deftest ^:benchmark pooled-carbonite-string-transmission-benchmarks
+;  (run-benchmarks
+;    "pooled-carbonite-string-transmission"
+;    create-pooled-carbonite-producer
+;    create-pooled-carbonite-consumer
+;    "foo-string"))
+;
+;(deftest ^:benchmark pooled-carbonite-lzf-string-transmission-benchmarks
+;  (run-benchmarks
+;    "pooled-carbonite-lzf-string-transmission"
+;    create-pooled-carbonite-lzf-producer
+;    create-pooled-carbonite-lzf-consumer
+;    "foo-string"))
 
 (def nippy-stress-data-benchable
   (dissoc taoensso.nippy/stress-data-benchable :lazy-seq :lazy-seq-empty :sorted-map :sorted-set))
