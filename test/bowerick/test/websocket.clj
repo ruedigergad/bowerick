b494564f3b6a1e1595ae2d06ae3a092f51ac49ce
Ruediger Gad <r.c.g@gmx.de>
2016-06-18 14:09:50 +0200

    Add first test for websocket support.

---
+++ test/bowerick/test/websocket.clj
@@ -0,0 +1,44 @@
+;;;
+;;;   Copyright 2016, Ruediger Gad
+;;;
+;;;   This software is released under the terms of the Eclipse Public License 
+;;;   (EPL) 1.0. You can find a copy of the EPL at: 
+;;;   http://opensource.org/licenses/eclipse-1.0.php
+;;;
+
+(ns 
+  ^{:author "Ruediger Gad",
+    :doc "Tests for JMS Interaction via Websockets"}  
+  bowerick.test.websocket
+  (:require
+    [bowerick.jms :refer :all]
+    [clj-assorted-utils.util :refer :all]
+    [clojure.test :refer :all] clojure.test))
+
+
+
+(def url-openwire "tcp://127.0.0.1:42424")
+(def url-websocket "ws://127.0.0.1:42425")
+(def test-topic "/topic/testtopic.foo")
+
+(defn test-with-broker [t]
+  (let [broker (start-broker [url-openwire url-websocket])]
+    (t)
+    (stop broker)))
+
+(use-fixtures :each test-with-broker)
+
+
+
+(deftest custom-transformation-producer-consumer-cheshire
+  (let [producer (create-producer url-websocket test-topic)
+        received (atom nil)
+        flag (prepare-flag)
+        consume-fn (fn [obj] (reset! received obj) (set-flag flag))
+        consumer (create-single-consumer url-openwire test-topic consume-fn)]
+    (producer "¿Que pasa?")
+    (await-flag flag)
+    (is (= "¿Que pasa?" @received))
+    (close producer)
+    (close consumer)))
+
