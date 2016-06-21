acee752f98432f6af0b60f06c48588edf28d3f7f
Ruediger Gad <r.c.g@gmx.de>
2016-06-21 09:23:09 +0200

    Prepare for client mode implementation.

--- src/bowerick/main.clj
+++ src/bowerick/main.clj
@@ -18,8 +18,29 @@
     [clojure.tools.cli :refer :all])
   (:gen-class))
 
+(defn start-broker-mode
+  [arg-map]
+  (println "Starting bowerick in broker mode.")
+  (let [url (arg-map :url)
+        broker-service (start-broker url)
+        shutdown-fn (fn []
+                      (stop broker-service))]
+    (if (:daemon arg-map)
+      (-> (agent 0) (await))
+      (do
+        (println "Broker started... Type \"q\" followed by <Return> to quit: ")
+        (while (not= "q" (read-line))
+          (println "Type \"q\" followed by <Return> to quit: "))
+        (println "Shutting down...")
+        (shutdown-fn)))))
+
+(defn start-client-mode
+  [arg-map]
+  (println "Starting bowerick in client mode."))
+
 (defn -main [& args]
   (let [cli-args (cli args
+                      ["-c" "--client" "Start in client mode." :flag true :default false]
                       ["-d" "--daemon" "Run as daemon." :flag true :default false]
                       ["-h" "--help" "Print this help." :flag true]
                       ["-u" "--url" 
@@ -33,19 +54,10 @@
     (when (arg-map :help)
       (println help-string)
       (System/exit 0))
-    (println "Starting ActiveMQ Broker using the following options:")
+    (println "Starting bowerick using the following options:")
     (pprint arg-map)
     (pprint extra-args)
-    (let [url (arg-map :url)
-          broker-service (start-broker url)
-          shutdown-fn (fn []
-                        (stop broker-service))]
-      (if (:daemon arg-map)
-        (-> (agent 0) (await))
-        (do
-          (println "Broker started... Type \"q\" followed by <Return> to quit: ")
-          (while (not= "q" (read-line))
-            (println "Type \"q\" followed by <Return> to quit: "))
-          (println "Shutting down...")
-          (shutdown-fn))))))
+    (if (:client arg-map)
+      (start-client-mode arg-map)
+      (start-broker-mode arg-map))))
 
