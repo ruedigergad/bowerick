96e82f232e2e21b2edcdac830320308fa4b24b53
Ruediger Gad <r.c.g@gmx.de>
2017-04-04 18:56:59 +0200

    Update version info.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject bowerick "1.99.14-SNAPSHOT"
+(defproject bowerick "1.99.14"
   :description "Easing Simple JMS Tasks with Clojure (and Java)"
   :dependencies [[com.twitter/carbonite "1.5.0"]
                  [cheshire "5.7.0"]
