7bf9827324e3f5aa7374133239479dec12781563
Ruediger Gad <r.c.g@gmx.de>
2017-10-01 21:46:56 +0200

    Add a-frame example that shows a trace.

---
+++ examples/simple_websocket_a-frame_example_trace/event_generator/project.clj
@@ -0,0 +1,9 @@
+(defproject event_generator "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [clj-assorted-utils "1.18.1"]
+                 [bowerick "1.99.17"]]
+  :main event-generator.main)
