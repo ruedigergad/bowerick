aa427209e7ee59b263eafaff54cec4a7dc33beae
Ruediger Gad <r.c.g@gmx.de>
2017-10-21 18:45:55 +0200

    Add A-Frame trace example to gh-pages.

---
+++ ghpages/examples/simple_websocket_a-frame_example_trace/event_generator/src/event_generator/main.clj
@@ -0,0 +1,38 @@
+;;;
+;;;   Copyright 2017, Ruediger Gad
+;;;
+;;;   This software is released under the terms of the Eclipse Public License 
+;;;   (EPL) 1.0. You can find a copy of the EPL at: 
+;;;   http://opensource.org/licenses/eclipse-1.0.php
+;;;
+
+(ns
+  ^{:author "Ruediger Gad",
+    :doc "Main class for a simple event generator for the A-Frame example."}
+  event-generator.main
+  (:require
+    [bowerick.jms :refer :all]
+    [clj-assorted-utils.util :refer :all]
+    [clojure.pprint :refer :all]
+    [clojure.string :as s])
+  (:import
+    [java.lang Math])
+  (:gen-class))
+
+(defn -main [& args]
+  (let [broker-url "ws://127.0.0.1:1864"
+        topic-name "/topic/aframe"
+        prod (create-producer broker-url topic-name 1)]
+    (loop [angle 0.0]
+      (let [x (Math/cos angle)
+            y (Math/sin angle)
+            max_angle (* 2.0 Math/PI)
+            angle_increment (/ max_angle 100.0)]
+        (println "x =" x "; y =" y)
+        (prod {:x x, :y y, :z 0})
+        (sleep 20)
+        (let [new_angle (+ angle angle_increment)]
+          (if (> new_angle max_angle)
+            (recur (+ 0.0 (- max_angle new_angle)))
+            (recur new_angle)))))))
+
