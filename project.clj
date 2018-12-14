480201b5f369025e2610e53fe6ae6960718df6ce
Ruediger Gad <r.c.g@gmx.de>
2018-12-14 20:54:26 +0100

    Update dependencies.

--- project.clj
+++ project.clj
@@ -1,28 +1,28 @@
-;(defproject bowerick "2.6.3-SNAPSHOT"
-(defproject bowerick "2.6.2"
+(defproject bowerick "2.6.3-SNAPSHOT"
+;(defproject bowerick "2.6.2"
   :description "Easing Simple JMS Tasks with Clojure (and Java)"
   :dependencies [[com.twitter/carbonite "1.5.0"]
-                 [cheshire "5.8.0"]
+                 [cheshire "5.8.1"]
                  [cli4clj "1.7.0"]
                  [clj-assorted-utils "1.18.2"]
                  [com.ning/compress-lzf "1.0.4"]
                  [com.taoensso/nippy "2.14.0"]
                  [javax.servlet/javax.servlet-api "4.0.1"]
-                 [org.apache.activemq/activemq-broker "5.15.6" :exclusions [org.eclipse.jetty.aggregate/jetty-all]]
-                 [org.apache.activemq/activemq-client "5.15.6" :exclusions [org.eclipse.jetty.aggregate/jetty-all]]
-                 [org.apache.activemq/activemq-http "5.15.6" :exclusions [org.eclipse.jetty.aggregate/jetty-all]]
-                 [org.apache.activemq/activemq-jaas "5.15.6" :exclusions [org.eclipse.jetty.aggregate/jetty-all]]
-                 [org.apache.activemq/activemq-openwire-legacy "5.15.6" :exclusions [org.eclipse.jetty.aggregate/jetty-all]]
-                 [org.apache.activemq/activemq-stomp "5.15.6" :exclusions [org.eclipse.jetty.aggregate/jetty-all]]
-                 [org.eclipse.jetty/jetty-server "9.4.11.v20180605"]
-                 [org.eclipse.jetty.websocket/websocket-client "9.4.11.v20180605"]
-                 [org.eclipse.jetty.websocket/websocket-server "9.4.11.v20180605"]
+                 [org.apache.activemq/activemq-broker "5.15.8" :exclusions [org.eclipse.jetty.aggregate/jetty-all]]
+                 [org.apache.activemq/activemq-client "5.15.8" :exclusions [org.eclipse.jetty.aggregate/jetty-all]]
+                 [org.apache.activemq/activemq-http "5.15.8" :exclusions [org.eclipse.jetty.aggregate/jetty-all]]
+                 [org.apache.activemq/activemq-jaas "5.15.8" :exclusions [org.eclipse.jetty.aggregate/jetty-all]]
+                 [org.apache.activemq/activemq-openwire-legacy "5.15.8" :exclusions [org.eclipse.jetty.aggregate/jetty-all]]
+                 [org.apache.activemq/activemq-stomp "5.15.8" :exclusions [org.eclipse.jetty.aggregate/jetty-all]]
+                 [org.eclipse.jetty/jetty-server "9.4.14.v20181114"]
+                 [org.eclipse.jetty.websocket/websocket-client "9.4.14.v20181114"]
+                 [org.eclipse.jetty.websocket/websocket-server "9.4.14.v20181114"]
                  [org.clojure/clojure "1.9.0"]
-                 [org.clojure/tools.cli "0.3.7"]
+                 [org.clojure/tools.cli "0.4.1"]
                  [org.eclipse.paho/org.eclipse.paho.client.mqttv3 "1.2.0"]
                  [org.iq80.snappy/snappy "0.4"]
-                 [org.springframework/spring-messaging "5.0.9.RELEASE"]
-                 [org.springframework/spring-websocket "5.0.9.RELEASE"]
+                 [org.springframework/spring-messaging "5.1.3.RELEASE"]
+                 [org.springframework/spring-websocket "5.1.3.RELEASE"]
                  [org.slf4j/slf4j-simple "1.7.25"]
                  [rgad/stompjms-client "1.20-SNAPSHOT"]]
   :license {:name "Eclipse Public License (EPL) - v 1.0"
@@ -42,7 +42,7 @@
   :html5-docs-ns-includes #"^bowerick.*"
   :html5-docs-repository-url "https://github.com/ruedigergad/bowerick/blob/master"
   :profiles {:repl
-               {:dependencies  [[jonase/eastwood "0.2.9" :exclusions  [org.clojure/clojure]]]}
+               {:dependencies  [[jonase/eastwood "0.3.4" :exclusions  [org.clojure/clojure]]]}
              :test
                {:dependencies [[criterium "0.4.4"]]
                 :test-paths ["test" "benchmark"]}}
