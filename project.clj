f743abc38547f4570af90f98598977e51df94aa8
Ruediger Gad <r.c.g@gmx.de>
2019-02-18 20:40:59 +0100

    Update springframework dependencies.

--- project.clj
+++ project.clj
@@ -21,8 +21,8 @@
                  [org.clojure/tools.cli "0.4.1"]
                  [org.eclipse.paho/org.eclipse.paho.client.mqttv3 "1.2.0"]
                  [org.iq80.snappy/snappy "0.4"]
-                 [org.springframework/spring-messaging "5.1.3.RELEASE"]
-                 [org.springframework/spring-websocket "5.1.3.RELEASE"]
+                 [org.springframework/spring-messaging "5.1.5.RELEASE"]
+                 [org.springframework/spring-websocket "5.1.5.RELEASE"]
                  [org.slf4j/slf4j-simple "1.7.25"]
                  [rgad/stompjms-client "1.20-SNAPSHOT"]]
   :license {:name "Eclipse Public License (EPL) - v 1.0"
