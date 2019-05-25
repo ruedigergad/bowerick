8436d82bd2062084a1e7ee2777554235cf8d91a9
Ruediger Gad <r.c.g@gmx.de>
2019-05-25 23:38:55 +0200

    Update dependency.

--- project.clj
+++ project.clj
@@ -19,7 +19,7 @@
                  [org.eclipse.jetty.websocket/websocket-server "9.4.18.v20190429"]
                  [org.clojure/clojure "1.10.0"]
                  [org.clojure/tools.cli "0.4.2"]
-                 [org.eclipse.paho/org.eclipse.paho.client.mqttv3 "1.2.0"]
+                 [org.eclipse.paho/org.eclipse.paho.client.mqttv3 "1.2.1"]
                  [org.iq80.snappy/snappy "0.4"]
                  [org.springframework/spring-messaging "5.1.7.RELEASE"]
                  [org.springframework/spring-websocket "5.1.7.RELEASE"]
