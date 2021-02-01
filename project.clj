d70c272dcc332bf6d5b234373079bda8b5f3ae2e
Ruediger Gad <r.c.g@gmx.de>
2021-02-01 20:03:00 +0100

    Revert to snapshot version.

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-;(defproject bowerick "2.7.1-SNAPSHOT"
-(defproject bowerick "2.7.0"
+(defproject bowerick "2.7.1-SNAPSHOT"
+;(defproject bowerick "2.7.0"
   :description "Easing Simple JMS Tasks with Clojure (and Java)"
   :dependencies [[com.twitter/carbonite "1.5.0"]
                  [cheshire "5.10.0"]
