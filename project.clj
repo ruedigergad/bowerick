5fa24173f9d9646fe7568e0b044d52caeece28d3
Ruediger Gad <r.c.g@gmx.de>
2021-05-23 18:32:18 +0200

    Update Eastwood dependency.

--- project.clj
+++ project.clj
@@ -43,7 +43,7 @@
   :html5-docs-ns-includes #"^bowerick.*"
   :html5-docs-repository-url "https://github.com/ruedigergad/bowerick/blob/master"
   :profiles {:repl
-               {:dependencies  [[jonase/eastwood "0.4.0" :exclusions  [org.clojure/clojure]]]}
+               {:dependencies  [[jonase/eastwood "0.4.2" :exclusions  [org.clojure/clojure]]]}
              :test
                {:dependencies [[criterium "0.4.6"]]
                 :test-paths ["test" "benchmark"]}}
