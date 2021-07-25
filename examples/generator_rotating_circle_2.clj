0ac36de5bcb13017f495558633f1b57e253823f9
Ruediger Gad <r.c.g@gmx.de>
2021-07-25 11:53:33 +0200

    Yin yang example.

--- examples/generator_rotating_circle_2.clj
+++ examples/generator_rotating_circle_2.clj
@@ -4,13 +4,38 @@
   (let [max_angle (* 2.0 Math/PI)
         angle_increment (/ max_angle 100.0)
         circle_steps (range 0.0 max_angle angle_increment)
+        half_circle_steps (range 0.0 Math/PI angle_increment)
         circle_coordinates (mapv (fn [angle]
                                    (let [x (Math/cos angle)
                                          y (Math/sin angle)]
                                      {"x" x, "y" y, "z" 0.0,
                                       "scale_x" 0.2, "scale_y" 0.2, "scale_z" 0.01,
                                       "rotation_x" 0.0, "rotation_y", 0.0, "rotation_z" 0.0}))
-                                 circle_steps)]
+                                 circle_steps)
+        upper_half_circle (mapv (fn [angle]
+                                  (let [x (/ (Math/sin angle) 2.0)
+                                        y (+ (/ (Math/cos angle) 2.0) 0.5)]
+                                    {"x" x, "y" y, "z" 0.0,
+                                     "scale_x" 0.2, "scale_y" 0.2, "scale_z" 0.01,
+                                     "rotation_x" 0.0, "rotation_y", 0.0, "rotation_z" 0.0}))
+                                half_circle_steps)
+        lower_half_circle (mapv (fn [angle]
+                                  (let [x (/ (Math/sin angle) -2.0)
+                                        y (- (/ (Math/cos angle) 2.0) 0.5)]
+                                    {"x" x, "y" y, "z" 0.0,
+                                     "scale_x" 0.2, "scale_y" 0.2, "scale_z" 0.01,
+                                     "rotation_x" 0.0, "rotation_y", 0.0, "rotation_z" 0.0}))
+                                half_circle_steps)
+        dots_coordinates [{"x" 0.0, "y" 0.5, "z" 0.0,
+                           "scale_x" 0.5, "scale_y" 0.5, "scale_z" 0.01,
+                           "rotation_x" 0.0, "rotation_y", 0.0, "rotation_z" 0.0}
+                          {"x" 0.0, "y" -0.5, "z" 0.0,
+                           "scale_x" 0.5, "scale_y" 0.5, "scale_z" 0.01,
+                           "rotation_x" 0.0, "rotation_y", 0.0, "rotation_z" 0.0}]
+        coordinates (concat circle_coordinates
+                            upper_half_circle
+                            lower_half_circle
+                            dots_coordinates)]
     (fn []
       (loop [rotation_angle 0.0]
         (let [rotated_coordinates (mapv (fn [coords]
@@ -32,7 +57,7 @@
                                               ["rotation_y"]
                                               (fn [_]
                                                 (- rotation_angle)))))
-                                        circle_coordinates)]
+                                        coordinates)]
           (producer rotated_coordinates)
           (delay-fn)
           (if (> rotation_angle max_angle)
