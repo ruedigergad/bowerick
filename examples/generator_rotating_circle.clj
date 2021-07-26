790a4758ab3bf6f2c6a8fd9c6cfdc306fc7c8522
Ruediger Gad <r.c.g@gmx.de>
2021-07-26 20:34:49 +0200

    Refactor to support re-loading at run-time.

--- examples/generator_rotating_circle.clj
+++ examples/generator_rotating_circle.clj
@@ -8,27 +8,28 @@
                                    (let [x (Math/cos angle)
                                          y (Math/sin angle)]
                                      {"x" x, "y" y, "z" 0.0}))
-                                 circle_steps)]
+                                 circle_steps)
+        rot_angle (atom 0.0)]
     (fn []
-      (loop [rotation_angle 0.0]
-        (let [rotated_coordinates (mapv (fn [coords]
-                                          (->
-                                            coords
-                                            (update-in
-                                              ["x"]
-                                              (fn [x z]
-                                                (- (* (Math/cos rotation_angle) x)
-                                                   (* (Math/sin rotation_angle) z)))
-                                              (coords "z"))
-                                            (update-in
-                                              ["z"]
-                                              (fn [z x]
-                                                (+ (* (Math/sin rotation_angle) x)
-                                                   (* (Math/cos rotation_angle) z)))
-                                              (coords "x"))))
-                                        circle_coordinates)]
-          (producer rotated_coordinates)
-          (delay-fn)
-          (if (> rotation_angle max_angle)
-            (recur (+ (- rotation_angle max_angle) angle_increment))
-            (recur (+ rotation_angle angle_increment))))))))
+      (let [rotation_angle @rot_angle
+            rotated_coordinates (mapv (fn [coords]
+                                        (->
+                                          coords
+                                          (update-in
+                                            ["x"]
+                                            (fn [x z]
+                                              (- (* (Math/cos rotation_angle) x)
+                                                 (* (Math/sin rotation_angle) z)))
+                                            (coords "z"))
+                                          (update-in
+                                            ["z"]
+                                            (fn [z x]
+                                              (+ (* (Math/sin rotation_angle) x)
+                                                 (* (Math/cos rotation_angle) z)))
+                                            (coords "x"))))
+                                      circle_coordinates)]
+        (producer rotated_coordinates)
+        (delay-fn)
+        (if (> rotation_angle max_angle)
+          (reset! rot_angle (+ (- rotation_angle max_angle) angle_increment))
+          (reset! rot_angle (+ rotation_angle angle_increment)))))))
