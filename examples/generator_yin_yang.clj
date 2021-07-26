9398f7f1cae1024df3aac24f90ff25728ae9fd5c
Ruediger Gad <r.c.g@gmx.de>
2021-07-26 08:52:13 +0200

    Fix file name in comment.

--- examples/generator_yin_yang.clj
+++ examples/generator_yin_yang.clj
@@ -1,5 +1,5 @@
 ; Run via, e.g.:
-; java -jar dist/bowerick-2.7.5-standalone.jar -G custom-fn -X examples/generator_rotating_circle.clj -I 20 -D /topic/aframe -u "ws://127.0.0.1:1864"
+; java -jar dist/bowerick-2.7.5-standalone.jar -G custom-fn -X examples/generator_yin_yang.clj -I 20 -D /topic/aframe -u "ws://127.0.0.1:1864"
 (fn [producer delay-fn]
   (let [max_angle (* 2.0 Math/PI)
         angle_increment (/ max_angle 100.0)
