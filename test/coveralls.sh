106c14c7b31d9c113356c1c0d7e3c43fcb786fce
Ruediger Gad <r.c.g@gmx.de>
2021-08-06 14:57:58 +0200

    Explicitly specify tests to be covered with cloverage.

--- test/coveralls.sh
+++ test/coveralls.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 COVERALLS_URL='https://coveralls.io/api/v1/jobs'
 
-lein cloverage -e "bowerick.JmsController" -e "bowerick.java-interfaces" -o cov --coveralls
+lein cloverage -e "bowerick.JmsController" -e "bowerick.java-interfaces" -t "bowerick.test.*" -o cov --coveralls
 ret=$?
 
 curl -F 'json_file=@cov/coveralls.json' "$COVERALLS_URL"
