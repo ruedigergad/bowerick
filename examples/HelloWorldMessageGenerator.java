787ec602cbe42b29d953c482848861d5db754b3e
Ruediger Gad <r.c.g@gmx.de>
2021-08-06 21:50:40 +0200

    Add brief howto as comment.

--- examples/HelloWorldMessageGenerator.java
+++ examples/HelloWorldMessageGenerator.java
@@ -1,5 +1,12 @@
 import bowerick.*;
 
+/*
+ * Compile with, e.g.:
+ * javac -cp ../dist/bowerick-2.8.0-standalone.jar HelloWorldMessageGenerator.java
+ *
+ * Run with, e.g.:
+ * java -jar ../dist/bowerick-2.8.0-standalone.jar -I 1000 -G custom-fn -X ./HelloWorldMessageGenerator.class
+ */
 class HelloWorldMessageGenerator implements MessageGenerator {
     public void generateMessage(JmsProducer producer) {
         producer.sendData("Hello World from Java");
