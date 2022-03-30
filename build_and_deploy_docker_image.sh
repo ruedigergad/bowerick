044727c768d6903a8d06f2a22205a16753405ce8
Ruediger Gad <r.c.g@gmx.de>
2022-03-30 08:23:06 +0200

    Add push commands.

--- build_and_deploy_docker_image.sh
+++ build_and_deploy_docker_image.sh
@@ -5,3 +5,6 @@ VERSION="2.9.5"
 docker build -t ruedigergad/bowerick:${VERSION} .
 docker tag ruedigergad/bowerick:${VERSION} ruedigergad/bowerick:latest
 
+docker push ruedigergad/bowerick:${VERSION}
+docker push ruedigergad/bowerick:latest
+
