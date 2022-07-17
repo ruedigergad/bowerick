78ab09392e923dc7644222c09d8009b0a9a026c4
Ruediger Gad <r.c.g@gmx.de>
2022-07-17 20:46:33 +0200

    Apply clj-kondo linting to benchmark.

--- benchmark/bowerick/benchmarks.clj
+++ benchmark/bowerick/benchmarks.clj
@@ -11,26 +11,23 @@
     :doc "Simple Benchmarks"}  
   bowerick.benchmarks
   (:require
-    [bowerick.jms :refer :all]
-    [clj-assorted-utils.util :refer :all]
-    [clojure.test :refer :all]
-    [criterium [core :as cc]])
-  (:import
-    (com.ning.compress.lzf LZFDecoder LZFEncoder)
-    (java.nio.charset Charset)))
+    [bowerick.jms :as jms]
+    [clojure.test :as test]
+    [criterium [core :as cc]]
+    [taoensso.nippy :as nippy]))
 
 (def ^:dynamic *local-jms-server* "tcp://127.0.0.1:42424")
 (def test-topic "/topic/testtopic.foo")
 
 (defn benchmark-fixture [t]
-  (let [broker (start-broker *local-jms-server*)]
+  (let [broker (jms/start-broker *local-jms-server*)]
     (t)
-    (stop broker)))
+    (jms/stop broker)))
 
-(use-fixtures :each benchmark-fixture)
+(test/use-fixtures :each benchmark-fixture)
 
 (def nippy-stress-data-benchable
-  (dissoc taoensso.nippy/stress-data-benchable :lazy-seq :lazy-seq-empty :sorted-map :sorted-set))
+  (dissoc nippy/stress-data-benchable :lazy-seq :lazy-seq-empty :sorted-map :sorted-set))
 
 (defn run-benchmarks
   [description producer-factory-fn consumer-factory-fn data]
@@ -41,38 +38,38 @@
       (cc/with-progress-reporting
         (cc/bench
           (producer data)))
-      (close producer)
-      (close consumer))))
+      (jms/close producer)
+      (jms/close consumer))))
 
-(deftest ^:benchmark default-serialization-nippy-stress-data-benchmarks
+(test/deftest ^:benchmark default-serialization-nippy-stress-data-benchmarks
   (run-benchmarks
     "default-serialization_nippy-stress-data"
-    create-producer
-    create-consumer
+    jms/create-producer
+    jms/create-consumer
     nippy-stress-data-benchable))
 
-;(deftest ^:benchmark pooled-cheshire-nippy-stress-data-transmission-benchmarks
+;(test/deftest ^:benchmark pooled-cheshire-nippy-stress-data-transmission-benchmarks
 ;  (run-benchmarks
 ;    "pooled-cheshire-nippy-stress-data-transmission"
 ;    (fn [url ep n]
-;      (create-pooled-producer url ep n cheshire.core/generate-string))
+;      (jms/create-pooled-producer url ep n cheshire.core/generate-string))
 ;    (fn [url ep cb]
-;      (create-pooled-consumer url ep cb cheshire.core/parse-string))
+;      (jms/create-pooled-consumer url ep cb cheshire.core/parse-string))
 ;    nippy-stress-data-benchable))
 ;
-;(deftest ^:benchmark pooled-cheshire-lzf-nippy-stress-data-transmission-benchmarks
+;(test/deftest ^:benchmark pooled-cheshire-lzf-nippy-stress-data-transmission-benchmarks
 ;  (let [charset (Charset/forName "UTF-8")]
 ;    (run-benchmarks
 ;      "pooled-cheshire-lzf-nippy-stress-data-transmission"
 ;      (fn [url ep n]
-;        (create-pooled-producer
+;        (jms/create-pooled-producer
 ;          url
 ;          ep
 ;          n
 ;          (fn [data]
 ;            (-> data (cheshire.core/generate-string) (.getBytes charset) (LZFEncoder/encode)))))
 ;      (fn [url ep cb]
-;        (create-pooled-consumer
+;        (jms/create-pooled-consumer
 ;          url
 ;          ep
 ;          cb
@@ -80,55 +77,57 @@
 ;            (-> data (LZFDecoder/decode) (String. charset) (cheshire.core/parse-string)))))
 ;      nippy-stress-data-benchable)))
 
-(deftest ^:benchmark nippy-serialization-nippy-stress-data-benchmarks
+(test/deftest ^:benchmark nippy-serialization-nippy-stress-data-benchmarks
   (run-benchmarks
     "nippy-serialization_nippy-stress-data"
-    create-nippy-producer
-    create-nippy-consumer
+    jms/create-nippy-producer
+    jms/create-nippy-consumer
     nippy-stress-data-benchable))
 
-(deftest ^:benchmark nippy-lz4-serialization-nippy-stress-data-benchmarks
+(test/deftest ^:benchmark nippy-lz4-serialization-nippy-stress-data-benchmarks
   (run-benchmarks
     "nippy-lz4-serialization_nippy-stress-data"
     (fn [url ep n]
-      (create-nippy-producer url ep n {:compressor taoensso.nippy/lz4-compressor}))
-    create-nippy-consumer
+      #_{:clj-kondo/ignore [:unresolved-var]}
+      (jms/create-nippy-producer url ep n {:compressor nippy/lz4-compressor}))
+    jms/create-nippy-consumer
     nippy-stress-data-benchable))
 
-(deftest ^:benchmark nippy-lzma2-serialization-nippy-stress-data-benchmarks
+(test/deftest ^:benchmark nippy-lzma2-serialization-nippy-stress-data-benchmarks
   (run-benchmarks
     "nippy-lzma2-serialization_nippy-stress-data"
     (fn [url ep n]
-      (create-nippy-producer url ep n {:compressor taoensso.nippy/lzma2-compressor}))
-    create-nippy-consumer
+      #_{:clj-kondo/ignore [:unresolved-var]}
+      (jms/create-nippy-producer url ep n {:compressor nippy/lzma2-compressor}))
+    jms/create-nippy-consumer
     nippy-stress-data-benchable))
 
-(deftest ^:benchmark nippy-snappy-serialization-nippy-stress-data-benchmarks
+(test/deftest ^:benchmark nippy-snappy-serialization-nippy-stress-data-benchmarks
   (run-benchmarks
     "nippy-snappy-serialization_nippy-stress-data"
     (fn [url ep n]
-      (create-nippy-producer url ep n {:compressor taoensso.nippy/snappy-compressor}))
-    create-nippy-consumer
+      #_{:clj-kondo/ignore [:unresolved-var]}
+      (jms/create-nippy-producer url ep n {:compressor nippy/snappy-compressor}))
+    jms/create-nippy-consumer
     nippy-stress-data-benchable))
 
-(deftest ^:benchmark nippy-lzf-serialization-nippy-stress-data-benchmarks
+(test/deftest ^:benchmark nippy-lzf-serialization-nippy-stress-data-benchmarks
   (run-benchmarks
     "nippy-lzf-serialization_nippy-stress-data"
-    create-nippy-lzf-producer
-    create-nippy-lzf-consumer
+    jms/create-nippy-lzf-producer
+    jms/create-nippy-lzf-consumer
     nippy-stress-data-benchable))
 
-(deftest ^:benchmark carbonite-serialization-nippy-stress-data-benchmarks
+(test/deftest ^:benchmark carbonite-serialization-nippy-stress-data-benchmarks
   (run-benchmarks
     "carbonite-serialization_nippy-stress-data"
-    create-carbonite-producer
-    create-carbonite-consumer
+    jms/create-carbonite-producer
+    jms/create-carbonite-consumer
     nippy-stress-data-benchable))
 
-(deftest ^:benchmark carbonite-lzf-serialization-nippy-stress-data-benchmarks
+(test/deftest ^:benchmark carbonite-lzf-serialization-nippy-stress-data-benchmarks
   (run-benchmarks
     "carbonite-lzf-serialization_nippy-stress-data"
-    create-carbonite-lzf-producer
-    create-carbonite-lzf-consumer
+    jms/create-carbonite-lzf-producer
+    jms/create-carbonite-lzf-consumer
     nippy-stress-data-benchable))
-
