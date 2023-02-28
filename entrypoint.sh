1a95cf119080fac755cba008ad75932116c621d2
Ruediger Gad <r.c.g@gmx.de>
2023-02-28 20:44:10 +0100

    Allways create message generation function.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,21 @@
 #!/bin/sh
 
+GEN_FN=${GEN_FN:-"
+(fn [producer delay-fn]
+  (let [max_angle (* 2.0 Math/PI)
+        angle_increment (/ max_angle 100.0)
+        angle (atom 0.0)]
+    (fn []
+      (let [x (Math/cos @angle)
+            y (Math/sin @angle)]
+        (producer (cheshire.core/generate-string [{\"x\" x, \"y\" y, \"z\" 0.0}]))
+        (delay-fn)
+        (if (> @angle max_angle)
+          (reset! angle (+ (- @angle max_angle) angle_increment))
+          (reset! angle (+ @angle angle_increment)))))))"
+}
+echo "${GEN_FN}" > generator.clj
+
 if [ -n "${CUSTOM_ARGS}" ] ;
 then
     echo "Running with custom args: ${CUSTOM_ARGS}"
@@ -24,21 +40,6 @@ GEN_DESTINATION=${GEN_DESTINATION:-/topic/bowerick.message.generator}
 GEN_TYPE=${GEN_TYPE:-custom-fn}
 GEN_ARGS=${GEN_ARGS:-./generator.clj}
 GEN_INTERVAL=${GEN_INTERVAL:-100}
-GEN_FN=${GEN_FN:-"
-(fn [producer delay-fn]
-  (let [max_angle (* 2.0 Math/PI)
-        angle_increment (/ max_angle 100.0)
-        angle (atom 0.0)]
-    (fn []
-      (let [x (Math/cos @angle)
-            y (Math/sin @angle)]
-        (producer (cheshire.core/generate-string [{\"x\" x, \"y\" y, \"z\" 0.0}]))
-        (delay-fn)
-        (if (> @angle max_angle)
-          (reset! angle (+ (- @angle max_angle) angle_increment))
-          (reset! angle (+ @angle angle_increment)))))))"
-}
-echo "${GEN_FN}" > generator.clj
 
 COMMAND="java -jar bowerick*standalone.jar -d -v"
 if ${BOOTSTRAP_CERTS} ;
